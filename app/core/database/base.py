b37290ef568b2c362c4e58fdabcab30569f2cb39
mks_min <mininname@vk.com>
2025-03-20 18:34:17 +0300

    Create Base class for models

---
+++ app/core/database/base.py
@@ -0,0 +1,17 @@
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+from sqlalchemy.ext.asyncio import AsyncAttrs
+
+from app.core import settings
+from app.utils import camel_case_converter
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention
+    )
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f'{camel_case_converter(cls.__name__)}s'
