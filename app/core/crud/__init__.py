cca5d9438a0ad4c4ed75fca3f00912c2b551828f
mks_min <mininname@vk.com>
2025-03-22 21:29:18 +0300

    Update import from app/core/crud/__init__

--- app/core/crud/__init__.py
+++ app/core/crud/__init__.py
@@ -1,11 +1,11 @@
 __all__ = (
     'connector',
-    'create_user_from_dict',
     'get_registration_stat',
-    'crud_manager'
+    'crud_manager',
+    'format_validation_error'
 )
 
 from .config import connector
-from .create import create_user_from_dict
 from .read import get_registration_stat
-from .create import crud_manager
+from .crud_manager import crud_manager, format_validation_error
+
