7679f450486f9112d608e33f0cf3074ed4d36716
mks_min <mininname@vk.com>
2025-03-25 17:49:46 +0300

    Code upgrade by black

--- app/api/api_v2/base_endpoints.py
+++ app/api/api_v2/base_endpoints.py
@@ -85,12 +85,18 @@ def verify_telegram_data(init_data: str) -> bool:
     Проверяет валидность данных от Telegram Web Apps
     """
     try:
-        vals = {k: unquote(v) for k, v in [s.split('=', 1) for s in init_data.split('&')]}
-        data_check_string = '\n'.join(f"{k}={v}" for k, v in sorted(vals.items()) if k != 'hash')
+        vals = {
+            k: unquote(v) for k, v in [s.split("=", 1) for s in init_data.split("&")]
+        }
+        data_check_string = "\n".join(
+            f"{k}={v}" for k, v in sorted(vals.items()) if k != "hash"
+        )
 
-        secret_key = hmac.new("WebAppData".encode(), settings.api.bot_token.encode(), hashlib.sha256).digest()
+        secret_key = hmac.new(
+            "WebAppData".encode(), settings.api.bot_token.encode(), hashlib.sha256
+        ).digest()
         h = hmac.new(secret_key, data_check_string.encode(), hashlib.sha256)
-        return h.hexdigest() == vals['hash']
+        return h.hexdigest() == vals["hash"]
     except Exception as e:
         print(f"Verification error: {e}")
         return False
