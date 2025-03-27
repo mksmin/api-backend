4ecd60aac119c85225c8a3921e9fc471f9f4bccb
mks_min <mininname@vk.com>
2025-03-27 13:10:18 +0300

    Create: New migration for project model

---
+++ alembic/versions/2025_03_27_1309-1e677c4b5e24_add_new_field_to_project_model.py
@@ -0,0 +1,46 @@
+"""Add new field to Project model
+
+Revision ID: 1e677c4b5e24
+Revises: 2358086814ea
+Create Date: 2025-03-27 13:09:30.978844
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "1e677c4b5e24"
+down_revision: Union[str, None] = "2358086814ea"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.add_column(
+        "projects",
+        sa.Column(
+            "prj_owner",
+            sa.BigInteger(),
+            nullable=True,
+            comment="tg_id пользователя",
+        ),
+    )
+    op.add_column(
+        "projects", sa.Column("prj_name", sa.String(length=50), nullable=True)
+    )
+    op.add_column(
+        "projects",
+        sa.Column("prj_description", sa.String(length=200), nullable=True),
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_column("projects", "prj_owner")
+    op.drop_column("projects", "prj_description")
+    op.drop_column("projects", "prj_name")
