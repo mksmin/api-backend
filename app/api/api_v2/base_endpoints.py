17fca4d430d69c8756746a4c81e60774685fb5a1
mks_min <mininname@vk.com>
2025-03-30 19:32:17 +0300

    Update: base_endpoints.py has new endpoint for user profile

--- app/api/api_v2/base_endpoints.py
+++ app/api/api_v2/base_endpoints.py
@@ -7,7 +7,8 @@ from urllib.parse import parse_qsl, unquote
 
 # import from lib
 from fastapi import APIRouter, Depends, Request, HTTPException
-from fastapi.responses import JSONResponse, FileResponse
+from fastapi.responses import JSONResponse, FileResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates
 from pathlib import Path
 
 # import from modules
@@ -20,6 +21,8 @@ FRONTEND_DIR = (
 )
 HTML_DIR = FRONTEND_DIR / "src"
 STATIC_DIR = FRONTEND_DIR / "public"
+
+templates = Jinja2Templates(directory=FRONTEND_DIR / "templates")
 
 # print(f"BASE_DIR: {BASE_DIR}")
 # print(f"FRONTEND_DIR: {FRONTEND_DIR}")
@@ -155,51 +158,113 @@ async def user_profile_tg(request: Request):
     }
     # pprint.pprint(f"params: {params}")
     profile_html = HTML_DIR / "profile.html"
+    profile_html = FRONTEND_DIR / "templates/base.html"
+
     return FileResponse(profile_html)
 
 
-def verify_telegram_data(init_data: str) -> bool:
+def verify_telegram_data(init_data: str) -> dict | bool:
     """
     Проверяет валидность данных от Telegram Web Apps
     """
     try:
-        vals = {
-            k: unquote(v) for k, v in [s.split("=", 1) for s in init_data.split("&")]
-        }
-        # print(f"vals: {vals}")
+        # Парсинг данных из initData
+        values = dict()
+        for splitted in init_data.split("&"):
+            key, value = splitted.split("=", 1)
+            values[key] = unquote(value)
+
+        # Проверка хэша
+        # Преобразование данных в строку для хэширования
         data_check_string = "\n".join(
-            f"{k}={v}" for k, v in sorted(vals.items()) if k != "hash"
+            f"{key}={value}" for key, value in sorted(values.items()) if key != "hash"
         )
-        # print(f"data_check_string: {data_check_string}")
+        # Генерация секретного ключа
         secret_key = hmac.new(
-            "WebAppData".encode(), settings.api.bot_token.encode(), hashlib.sha256
+            "WebAppData".encode(),
+            settings.api.bot_token.encode(),
+            hashlib.sha256,
         ).digest()
-        h = hmac.new(secret_key, data_check_string.encode(), hashlib.sha256)
-        # print("hash: ", h.hexdigest())
-        # print("vals: ", vals["hash"])
 
-        return h.hexdigest() == vals["hash"]
+        # Генерация хэша
+        generated_hash = hmac.new(
+            secret_key,
+            data_check_string.encode(),
+            hashlib.sha256,
+        ).hexdigest()
+
+        # Защита от атаки по времени
+        if not hmac.compare_digest(generated_hash, values["hash"]):
+            return False
+
+        # Парсинг данных из initData
+        user_data = json.loads(values.get("user", "{}"))
+        required_fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "username",
+            "language_code",
+            "is_premium",
+            "photo_url",
+        ]
+        pprint.pprint(f"user_data: {user_data}")
+        if not all(field in user_data for field in required_fields):
+            print(
+                f"Required fields missing: {[field for field in required_fields if field not in user_data]}"
+            )
+            return False
+        return {
+            "id": user_data["id"],
+            "first_name": user_data["first_name"],
+            "last_name": user_data["last_name"],
+            "username": user_data["username"],
+            "is_premium": user_data["is_premium"],
+            "photo_url": user_data["photo_url"],
+            "language_code": user_data["language_code"],
+            "allows_write_to_pm": user_data["allows_write_to_pm"],
+        }
+
+        # vals = {
+        #     k: unquote(v) for k, v in [s.split("=", 1) for s in init_data.split("&")]
+        # }
+        #
+        # print(f"vals: {vals}")
+        # print(f"values: {values}")
+        # data_check_string2 = "\n".join(
+        #     f"{k}={v}" for k, v in sorted(vals.items()) if k != "hash"
+        # )
+        # secret_key2 = hmac.new(
+        #     "WebAppData".encode(), settings.api.bot_token.encode(), hashlib.sha256
+        # ).digest()
+        #
+        # h = hmac.new(secret_key2, data_check_string2.encode(), hashlib.sha256)
+
+        # print(f"h.hexdigest(): {h.hexdigest()}")
+        # return h.hexdigest() == vals["hash"]
+
     except Exception as e:
-        print(f"Verification error: {e}")
-        return False
+        raise ValueError(f"Verification error: {e}")
+        # return False
 
 
-@router.post("/verify")
+@router.post("/verify", response_class=HTMLResponse)
 async def verify_telegram(request: Request):
     try:
+        # Получение init данных из запроса
         data = await request.json()
-        # print(f"data: {data}")
-
         init_data = data.get("initData")
-        # print(f"init_data: {init_data}")
 
         if not init_data:
             raise HTTPException(status_code=400, detail="Missing initData")
 
-        if verify_telegram_data(init_data):
-            return {"status": "success", "message": "Data verified"}
-        else:
+        user_data = verify_telegram_data(init_data)
+        if not user_data:
             raise HTTPException(status_code=401, detail="Invalid data")
+
+        return templates.TemplateResponse(
+            "profile.html", {"request": request, "user": user_data}
+        )
 
     except Exception as e:
         logger.error(f"Error: {e}")
