37ff06c39089b23cf28926cebbc8c2365dea272b
mks_min <mininname@vk.com>
2025-04-06 19:26:30 +0300

    Migrations: add new migrations for users table

---
+++ alembic/versions/2025_04_06_1924-fdd633cd3736_add_uuid_username_fields.py
@@ -0,0 +1,35 @@
+"""Add uuid, username fields
+
+Revision ID: fdd633cd3736
+Revises: 11c6fc0e266c
+Create Date: 2025-04-06 19:24:45.379879
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "fdd633cd3736"
+down_revision: Union[str, None] = "11c6fc0e266c"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.add_column("users", sa.Column("uuid", sa.UUID(), nullable=True))
+    op.add_column("users", sa.Column("username", sa.String(length=100), nullable=True))
+    op.create_index(op.f("ix_users_tg_id"), "users", ["tg_id"], unique=False)
+    op.create_unique_constraint(op.f("uq_users_uuid"), "users", ["uuid"])
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_constraint(op.f("uq_users_uuid"), "users", type_="unique")
+    op.drop_index(op.f("ix_users_tg_id"), table_name="users")
+    op.drop_column("users", "username")
+    op.drop_column("users", "uuid")
