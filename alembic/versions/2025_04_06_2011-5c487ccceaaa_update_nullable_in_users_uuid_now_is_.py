98d67b52e64713645ff0b1e2d87465e86b402034
mks_min <mininname@vk.com>
2025-04-06 20:12:00 +0300

    Migrations: add new migrations for users table

---
+++ alembic/versions/2025_04_06_2011-5c487ccceaaa_update_nullable_in_users_uuid_now_is_.py
@@ -0,0 +1,29 @@
+"""Update nullable in users.uuid. Now is False
+
+Revision ID: 5c487ccceaaa
+Revises: e9300def0c06
+Create Date: 2025-04-06 20:11:27.667869
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "5c487ccceaaa"
+down_revision: Union[str, None] = "e9300def0c06"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.alter_column("users", "uuid", existing_type=sa.UUID(), nullable=False)
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.alter_column("users", "uuid", existing_type=sa.UUID(), nullable=True)
