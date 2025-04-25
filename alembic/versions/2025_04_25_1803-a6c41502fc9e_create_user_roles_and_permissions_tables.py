dc6edb91885aded2fa86b3cf2c23641025434619
mks_min <mininname@vk.com>
2025-04-25 18:07:00 +0300

    Create: new alembic migration for new tables

---
+++ alembic/versions/2025_04_25_1803-a6c41502fc9e_create_user_roles_and_permissions_tables.py
@@ -0,0 +1,77 @@
+"""Create user roles and permissions tables
+
+Revision ID: a6c41502fc9e
+Revises: a1f1d1d48ba6
+Create Date: 2025-04-25 18:03:48.640651
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "a6c41502fc9e"
+down_revision: Union[str, None] = "a1f1d1d48ba6"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "permissions",
+        sa.Column("id", sa.Integer(), autoincrement=True, nullable=False),
+        sa.Column("code", sa.String(length=100), nullable=False),
+        sa.Column("description", sa.String(length=200), nullable=True),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_permissions")),
+        sa.UniqueConstraint("code", name=op.f("uq_permissions_code")),
+    )
+    op.create_table(
+        "roles",
+        sa.Column("id", sa.Integer(), autoincrement=True, nullable=False),
+        sa.Column("name", sa.String(length=50), nullable=False),
+        sa.Column("description", sa.String(length=200), nullable=True),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_roles")),
+        sa.UniqueConstraint("name", name=op.f("uq_roles_name")),
+    )
+    op.create_table(
+        "role_permissions",
+        sa.Column("role_id", sa.Integer(), nullable=False),
+        sa.Column("permission_id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["permission_id"],
+            ["permissions.id"],
+            name=op.f("fk_role_permissions_permission_id_permissions"),
+        ),
+        sa.ForeignKeyConstraint(
+            ["role_id"],
+            ["roles.id"],
+            name=op.f("fk_role_permissions_role_id_roles"),
+        ),
+        sa.PrimaryKeyConstraint(
+            "role_id", "permission_id", name=op.f("pk_role_permissions")
+        ),
+    )
+    op.create_table(
+        "user_roles",
+        sa.Column("user_id", sa.Integer(), nullable=False),
+        sa.Column("role_id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["role_id"], ["roles.id"], name=op.f("fk_user_roles_role_id_roles")
+        ),
+        sa.ForeignKeyConstraint(
+            ["user_id"], ["users.id"], name=op.f("fk_user_roles_user_id_users")
+        ),
+        sa.PrimaryKeyConstraint("user_id", "role_id", name=op.f("pk_user_roles")),
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table("user_roles")
+    op.drop_table("role_permissions")
+    op.drop_table("roles")
+    op.drop_table("permissions")
