43a94d16e36d020cb4c774cb601fdfc3d72adb7c
mks_min <mininname@vk.com>
2025-04-27 20:33:36 +0300

    Create: new migration for projects table

---
+++ alembic/versions/2025_04_27_2031-471d42f906f5_create_fk_in_projects_table.py
@@ -0,0 +1,53 @@
+"""Create FK in Projects table
+
+Revision ID: 471d42f906f5
+Revises: a6c41502fc9e
+Create Date: 2025-04-27 20:31:47.626156
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "471d42f906f5"
+down_revision: Union[str, None] = "a6c41502fc9e"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.alter_column(
+        "projects",
+        "prj_owner",
+        existing_type=sa.BIGINT(),
+        comment="id пользователя",
+        existing_comment="tg_id пользователя",
+        existing_nullable=False,
+    )
+    op.create_foreign_key(
+        op.f("fk_projects_prj_owner_users"),
+        "projects",
+        "users",
+        ["prj_owner"],
+        ["id"],
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_constraint(
+        op.f("fk_projects_prj_owner_users"), "projects", type_="foreignkey"
+    )
+    op.alter_column(
+        "projects",
+        "prj_owner",
+        existing_type=sa.BIGINT(),
+        comment="tg_id пользователя",
+        existing_comment="id пользователя",
+        existing_nullable=False,
+    )
