1f63abf4afcc37c43bbf74adf79a634fa99796b3
mks_min <mininname@vk.com>
2025-05-11 17:42:31 +0300

    feat(db): add new column to projects table

--- app/core/database/projects.py
+++ app/core/database/projects.py
@@ -1,7 +1,7 @@
 import uuid
 
 from sqlalchemy import UUID, String, BigInteger, ForeignKey
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from app.core.database import Base
 from app.core.database.mixins import IntIdMixin, TimestampsMixin
@@ -17,5 +17,8 @@ class Project(IntIdMixin, TimestampsMixin, Base):
     prj_name: Mapped[str] = mapped_column(String(50), nullable=True)
     prj_description: Mapped[str] = mapped_column(String(200), nullable=True)
     prj_owner = mapped_column(
-        BigInteger, ForeignKey("users.id"), nullable=False, comment="id пользователя"
-    )
+        BigInteger, ForeignKey("users.id"), nullable=True, comment="id пользователя")
+    parent_id: Mapped[int] = mapped_column(BigInteger, ForeignKey('projects.id'), nullable=True,
+                                           comment="id родительского проекта")
+
+    parent: Mapped["Project"] = relationship("Project", remote_side='Project.id', backref="subprojects", lazy="joined")
