926f87e8a108f4a736e53ee05c9ca9bf55179bb7
mks_min <mininname@vk.com>
2025-05-26 12:26:45 +0300

    refactor(apikeys): rename API key generator and update imports
    Renamed the function for generating API keys to improve clarity.
    Updated __init__.py imports accordingly.
    Added a docstrings to the function for better documentation.

--- app/api/api_v2/auth/__init__.py
+++ app/api/api_v2/auth/__init__.py
@@ -1,5 +1,11 @@
-__all__ = ("auth_utils", "token_utils", "auth_router")
+__all__ = (
+    "auth_utils",
+    "token_utils",
+    "auth_router",
+    "api_key_router",
+)
 
 from . import auth_handler as auth_utils
 from . import access_token_helper as token_utils
 from .auth_endpoints import router as auth_router
+from .api_key_endpoints import router as api_key_router
--- app/core/database/security/__init__.py
+++ app/core/database/security/__init__.py
@@ -1,7 +1,7 @@
 __all__ = (
     "APIKey",
-    "generate_api_key",
+    "generate_api_key_and_hash",
 )
 
 from .models import APIKey
-from .utils import generate_api_key
+from .utils import generate_api_key_and_hash
