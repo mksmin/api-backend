56b2e3c249a206cb72d80d75b1d7bf60b53edc6c
mks_min <mininname@vk.com>
2025-05-26 19:34:35 +0300

    refactor(crud_manager): move BaseCRUDManager to base module

--- app/core/crud/crud_manager.py
+++ app/core/crud/crud_manager.py
@@ -1,21 +1,17 @@
 # import lib
-from contextlib import asynccontextmanager
-from datetime import datetime
 
 from pydantic import ValidationError, UUID4
-from sqlalchemy import select, update, and_
-from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import select, and_
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
-from typing import TypeVar, Generic, Type, AsyncIterator, cast
+from typing import TypeVar
 
 # import from modules
 from app.core.config import logger
 from app.core.database import User, Project
 from app.core.database.schemas import UserSchema, ProjectSchema
+from .managers import BaseCRUDManager, ModelType, APIKeyManager
 
 from .. import db_helper
-
-ModelType = TypeVar("ModelType")
 
 
 def format_validation_error(exc: ValidationError) -> str:
@@ -25,78 +21,6 @@ def format_validation_error(exc: ValidationError) -> str:
         msg = error["msg"]
         errors.append(f"{field}: {msg}")
     return "; ".join(errors)
-
-
-class BaseCRUDManager(Generic[ModelType]):
-    def __init__(
-        self, session_factory: async_sessionmaker[AsyncSession], model: Type[ModelType]
-    ):
-        self.session_factory = session_factory
-        self.model = model
-
-    @asynccontextmanager
-    async def _get_session(self) -> AsyncIterator[AsyncSession]:
-        async with self.session_factory() as session:
-            try:
-                yield session
-                await session.commit()
-            except Exception:
-                await session.rollback()
-                logger.exception("Ошибка при работе с базой данных")
-                raise
-
-    async def exists_by_field(self, field: str, value: str | int) -> bool | ModelType:
-        async with self._get_session() as session:
-            session = cast(
-                AsyncSession, session
-            )  # asynccontextmanager не передает аннотацию AsyncSession, поэтому явно указываем
-            query = select(self.model).where(getattr(self.model, field) == value)
-            result = await session.execute(query)
-            return result.scalar_one_or_none() is not None
-
-    async def create(self, **kwargs) -> ModelType:
-        async with self._get_session() as session:
-            session = cast(AsyncSession, session)
-            instance = self.model(**kwargs)
-            session.add(instance)
-            await session.flush()
-            await session.refresh(instance)
-            logger.info(f"Created {self.model.__name__} with id: {instance.id}")
-            return instance
-
-    async def get_one(self, field: str, value: str | int) -> ModelType | None:
-        result = await self.exists_by_field(field, value)
-        if result:
-            async with self._get_session() as session:
-                query = select(self.model).where(getattr(self.model, field) == value)
-                result = await session.execute(query)
-                return result.scalar_one_or_none()
-
-        return result
-
-    async def delete(self, field: str, value: str | int) -> None:
-        async with self._get_session() as session:
-            query = (
-                update(self.model)
-                .where(
-                    and_(
-                        getattr(self.model, field) == value,
-                        self.model.deleted_at.is_(None),
-                    )
-                )
-                .values(deleted_at=datetime.now())
-            )
-            try:
-                result = await session.execute(query)
-                if result.rowcount == 0:
-                    if await self.exists_by_field(field, value):
-                        raise ValueError(f"Объект с {field} = {value} уже удален")
-                    else:
-                        raise ValueError(f"Объект с {field} = {value} не найден")
-
-            except SQLAlchemyError as e:
-                await session.rollback()
-                raise RuntimeError(f"Ошибка при удалении объекта: {str(e)}")
 
 
 class UserManager(BaseCRUDManager[User]):
@@ -196,8 +120,11 @@ class ProjectManager(BaseCRUDManager[Project]):
             result = await session.execute(query)
             project = result.scalar_one_or_none()
             if not project:
+                logger.error(
+                    f"Проект с id = {project_id} / uuid = {str(project_uuid)[:8]} не найден у пользователя {owner_id}"
+                )
                 raise ValueError(
-                    f"Проект с id = {project_id} не найден у пользователя {owner_id}, либо удален."
+                    f"Проект с id = {project_id} / uuid = {str(project_uuid)[:8]} не найден у пользователя {owner_id}, либо удален."
                 )
             return [project]
 
@@ -217,6 +144,7 @@ class CRUDManager:
     def __init__(self, session_factory: async_sessionmaker):
         self.user: UserManager = UserManager(session_factory)
         self.project: ProjectManager = ProjectManager(session_factory)
+        self.api_keys: APIKeyManager = APIKeyManager(session_factory)
 
 
 crud_manager = CRUDManager(db_helper.session_factory)
---
+++ app/core/crud/managers/base.py
@@ -0,0 +1,83 @@
+from contextlib import asynccontextmanager
+from datetime import datetime
+from typing import Generic, Type, AsyncIterator, cast, TypeVar
+
+from sqlalchemy import select, update, and_
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
+
+from app.core import logger
+
+ModelType = TypeVar("ModelType")
+
+
+class BaseCRUDManager(Generic[ModelType]):
+    def __init__(
+        self, session_factory: async_sessionmaker[AsyncSession], model: Type[ModelType]
+    ):
+        self.session_factory = session_factory
+        self.model = model
+
+    @asynccontextmanager
+    async def _get_session(self) -> AsyncIterator[AsyncSession]:
+        async with self.session_factory() as session:
+            try:
+                yield session
+                await session.commit()
+            except Exception:
+                await session.rollback()
+                logger.exception("Ошибка при работе с базой данных")
+                raise
+
+    async def exists_by_field(self, field: str, value: str | int) -> bool | ModelType:
+        async with self._get_session() as session:
+            session = cast(
+                AsyncSession, session
+            )  # asynccontextmanager не передает аннотацию AsyncSession, поэтому явно указываем
+            query = select(self.model).where(getattr(self.model, field) == value)
+            result = await session.execute(query)
+            return result.scalar_one_or_none() is not None
+
+    async def create(self, **kwargs) -> ModelType:
+        async with self._get_session() as session:
+            session = cast(AsyncSession, session)
+            instance = self.model(**kwargs)
+            session.add(instance)
+            await session.flush()
+            await session.refresh(instance)
+            logger.info(f"Created {self.model.__name__} with id: {instance.id}")
+            return instance
+
+    async def get_one(self, field: str, value: str | int) -> ModelType | None:
+        result = await self.exists_by_field(field, value)
+        if result:
+            async with self._get_session() as session:
+                query = select(self.model).where(getattr(self.model, field) == value)
+                result = await session.execute(query)
+                return result.scalar_one_or_none()
+
+        return result
+
+    async def delete(self, field: str, value: str | int) -> None:
+        async with self._get_session() as session:
+            query = (
+                update(self.model)
+                .where(
+                    and_(
+                        getattr(self.model, field) == value,
+                        self.model.deleted_at.is_(None),
+                    )
+                )
+                .values(deleted_at=datetime.now())
+            )
+            try:
+                result = await session.execute(query)
+                if result.rowcount == 0:
+                    if await self.exists_by_field(field, value):
+                        raise ValueError(f"Объект с {field} = {value} уже удален")
+                    else:
+                        raise ValueError(f"Объект с {field} = {value} не найден")
+
+            except SQLAlchemyError as e:
+                await session.rollback()
+                raise RuntimeError(f"Ошибка при удалении объекта: {str(e)}")
