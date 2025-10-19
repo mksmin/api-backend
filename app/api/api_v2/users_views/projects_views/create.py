164740ceecd2658e76a85f9dffd27779b01c205d
mks_min <mininname@vk.com>
2025-10-19 16:11:08 +0300

    feat: update create view with crud service

--- app/api/api_v2/users_views/projects_views/create.py
+++ app/api/api_v2/users_views/projects_views/create.py
@@ -1,3 +1,5 @@
+from typing import Annotated
+
 from fastapi import (
     APIRouter,
     Depends,
@@ -6,26 +8,49 @@ from fastapi import (
 )
 
 from api.api_v2.auth import token_utils
+from app_exceptions import InvalidUUIDError, UserNotFoundError
+from app_exceptions.exceptions import ProjectAlreadyExistsError
 from core.crud import crud_manager
-from core.database.schemas import ProjectRequestSchema, ProjectResponseSchema
+from core.crud.crud_service import GetCRUDService
 from core.database.security import schemas as ak_schemas
+from schemas import ProjectCreateSchema, ProjectReadSchema
 
 router = APIRouter()
 
 
 @router.post(
     "/",
-    dependencies=[
-        Depends(token_utils.strict_validate_access_token),
-    ],
 )
 async def create_project(
-    project_create: ProjectRequestSchema,
-) -> ProjectResponseSchema:
-    project = await crud_manager.project.create(
-        project_create.model_dump(),
-    )
-    return ProjectResponseSchema.model_validate(project)
+    crud_service: GetCRUDService,
+    project_create: ProjectCreateSchema,
+    user_id: Annotated[
+        str,
+        Depends(token_utils.strict_validate_access_token),
+    ],
+) -> ProjectReadSchema:
+    try:
+        result = await crud_service.project.create_project(
+            project_create=project_create,
+            user_id=int(user_id),
+        )
+    except UserNotFoundError as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        ) from e
+    except InvalidUUIDError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid UUID",
+        ) from e
+    except ProjectAlreadyExistsError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Project already exists",
+        ) from e
+    else:
+        return result
 
 
 @router.post(
