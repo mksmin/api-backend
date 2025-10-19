c545100314c108e11fa929dc13394685601435e0
mks_min <mininname@vk.com>
2025-10-19 05:07:45 +0300

    feat: create user schemas

---
+++ app/schemas/__init__.py
@@ -0,0 +1,9 @@
+from schemas.user import (
+    UserCreateSchema as UserCreateSchema,
+)
+from schemas.user import (
+    UserReadSchema as UserReadSchema,
+)
+from schemas.user import (
+    UserSchema as UserSchema,
+)
