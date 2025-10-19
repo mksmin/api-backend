9405e3f68fffb27afdfc5c76243bc6a262f2737d
mks_min <mininname@vk.com>
2025-10-19 23:25:08 +0300

    feat: project not found exception

--- app/app_exceptions/__init__.py
+++ app/app_exceptions/__init__.py
@@ -1,2 +1,6 @@
 from app_exceptions.exceptions import InvalidUUIDError as InvalidUUIDError
+from app_exceptions.exceptions import (
+    ProjectAlreadyExistsError as ProjectAlreadyExistsError,
+)
+from app_exceptions.exceptions import ProjectNotFoundError as ProjectNotFoundError
 from app_exceptions.exceptions import UserNotFoundError as UserNotFoundError
