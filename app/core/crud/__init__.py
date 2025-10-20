3458dde8e326a3426f0e24462ddae91b164bd172
mks_min <mininname@vk.com>
2025-10-20 18:16:01 +0300

    refactor: remove unused func

--- app/core/crud/__init__.py
+++ app/core/crud/__init__.py
@@ -1,10 +1,3 @@
-__all__ = (
-    "GetCRUDService",
-    "connector",
-    "get_registration_stat",
-)
+__all__ = ("GetCRUDService",)
 
 from core.crud.crud_service import GetCRUDService
-
-from .config import connector
-from .read import get_registration_stat
