6ef97f6061b16cfb33c19eab432f342135f57cc0
mks_min <mininname@vk.com>
2025-10-23 17:00:02 +0300

    feat: views for projects pages

--- app/rest/pages_views/page_views.py
+++ app/rest/pages_views/page_views.py
@@ -2,9 +2,16 @@ from typing import Annotated, Any
 
 from fastapi import APIRouter, Depends
 from fastapi.responses import HTMLResponse
+from starlette.requests import Request
 
+from api.api_v2.users_views.projects_views.dependencies import (
+    get_project_by_uuid,
+    get_user_projects,
+)
 from core.config import settings
+from core.database.schemas import ProjectResponseSchema
 from paths_constants import templates
+from schemas import ProjectReadSchema
 
 from .dependencies import (
     get_dict_with_user_affirmations,
@@ -49,6 +56,56 @@ async def page_profile(
     return templates.TemplateResponse(
         "profiles/profile.html",
         template_data,
+    )
+
+
+@router.get(
+    "/projects2/{project_uuid}",
+    name="user-projects:detail",
+    include_in_schema=settings.run.dev_mode,
+)
+async def get_list_projects(
+    request: Request,
+    project: Annotated[
+        ProjectResponseSchema,
+        Depends(get_project_by_uuid),
+    ],
+) -> HTMLResponse:
+    """Страница с профилем пользователя"""
+    return templates.TemplateResponse(
+        "project/details.html",
+        context={
+            "request": request,
+            "project": project.model_dump(),
+        },
+    )
+
+
+@router.get(
+    "/projects2",
+    name="user-projects:list",
+    include_in_schema=settings.run.dev_mode,
+)
+async def get_page_detail_project(
+    request: Request,
+    projects: Annotated[
+        list[ProjectReadSchema],
+        Depends(get_user_projects),
+    ],
+    template_data: Annotated[
+        dict[str, Any],
+        Depends(return_data_for_user_profile_template),
+    ],
+) -> HTMLResponse:
+    """Страница с профилем пользователя"""
+    context = {
+        "request": request,
+        "projects": projects,
+        "user": template_data.get("user"),
+    }
+    return templates.TemplateResponse(
+        "project/list.html",
+        context=context,
     )
 
 
