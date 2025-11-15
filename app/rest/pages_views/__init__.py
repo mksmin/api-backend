0ace375be37da7a50202734f1d8c2c450bf67616
mks_min <mininname@vk.com>
2025-11-15 22:03:06 +0300

    refactor: rename module

--- app/rest/pages_views/__init__.py
+++ app/rest/pages_views/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from misc.rabbitmq_broker import rabbitmq_broker
-from rest.pages_views.affirmations_view import router as affirmations_router
+from rest.pages_views.affirmations_views import router as affirmations_router
 from rest.pages_views.login_views import router as auth_router
 from rest.pages_views.projects_views import router as projects_router
 
