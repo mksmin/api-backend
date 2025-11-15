98241612ae5712cae306e5d0a608ee0d0eabd0e8
mks_min <mininname@vk.com>
2025-11-15 22:32:17 +0300

    refactor: move view to new module

---
+++ app/rest/pages_views/profile_views.py
@@ -0,0 +1,34 @@
+from typing import Annotated, Any
+
+from fastapi import APIRouter, Depends
+from fastapi.responses import HTMLResponse
+
+from core.config import settings
+from paths_constants import templates
+from rest.pages_views.dependencies.user_data import (
+    return_data_for_user_profile_template,
+)
+from rest.pages_views.redirect import redirect_to_login_page
+
+router = APIRouter()
+
+
+@router.get(
+    "/profile",
+    name="profiles:user-profile",
+    include_in_schema=settings.run.dev_mode,
+    dependencies=[
+        Depends(redirect_to_login_page),
+    ],
+)
+async def page_profile(
+    template_data: Annotated[
+        dict[str, Any],
+        Depends(return_data_for_user_profile_template),
+    ],
+) -> HTMLResponse:
+    """Страница с профилем пользователя"""
+    return templates.TemplateResponse(
+        "profiles/profile.html",
+        template_data,
+    )
